63b024db646e268408f56b49f72b1c14a06de2ce
Murali Meena <gopalakrishm@ornl.gov>
2025-01-21 09:29:25 -0500

    Edited custom output T; other minor edits

--- archs/MLP_transformer.py
+++ archs/MLP_transformer.py
@@ -6,8 +6,8 @@ class MLP_transformer(nn.Module):
     def __init__(self, input_shape, output_shape, units1=2000, units2=8000, units3=2000, activation='elu', num_heads=8, num_layers=6, dim_feedforward=16, dropout=0.1):
         super(MLP_transformer, self).__init__()
         """
-        input: [B,T,C,Samples]
-        output: [B,T,C,H,W,D]
+        input: [B, T, C, Samples]
+        output: [B, T', C', H, W, D]
         """
         print(f"Input shape: {input_shape}; output shape: {output_shape}")
         self.output_shape = output_shape
@@ -44,7 +44,7 @@ class MLP_transformer(nn.Module):
 
         # Decoder (CNN)
         in_channel = self.input_shape[-3] * dim_feedforward  # T*embd
-        out_channel = self.output_shape[-5] * self.output_shape[-4]  # T*C
+        out_channel = self.output_shape[-5] * self.output_shape[-4]  # T'*C'
         self.conv_transpose = nn.Sequential(
                                             nn.ConvTranspose3d(
                                                 in_channels=in_channel,
@@ -73,19 +73,20 @@ class MLP_transformer(nn.Module):
         in_channel = self.conv_transpose[0].in_channels
         x = x.reshape(batch_size, in_channel, 1, 1, 1) # [B, T*emb, 1, 1, 1]
         # loop over ConvTranspose
-        x = self.conv_transpose(x) # [B, T*emb, 1, 1, 1] -> [B, T*C, H, W, D]
-        # Reshape to [B, T, C, H, W, D]
+        x = self.conv_transpose(x) # [B, T*emb, 1, 1, 1] -> [B, T'*C', H, W, D]
+        # Reshape to [B, T', C', H, W, D]
         out_H, out_W, out_D = self.conv_transpose[0].kernel_size
         channels = self.output_shape[-4]
+        time = self.output_shape[-5]
         x = x.reshape(batch_size, time, channels, out_H, out_W, out_D)
 
         return x
 
 def build_model(input_shape, output_shape, units1=2000, units2=8000, units3=2000, activation='elu', window=1, num_heads=8, num_layers=6, dim_feedforward=16, dropout=0.1):
-    model = MLP_transformer(input_shape, output_shape, units1, units2, units3, activation, num_heads, num_layers, dim_feedforward, dropout)
+    model = MLP_transformer(input_shape, output_shape, units1=units1, units2=units2, units3=units3, activation=activation, num_heads=num_heads, num_layers=num_layers, dim_feedforward=dim_feedforward, dropout=dropout)
     return model
 
-def get_meta_model(input_shape):
+def get_meta_model(input_shape, output_shape):
     def meta_model(hp):
         units1 = hp.Int('units1', min_value=2, max_value=8000, step=800)
         units2 = hp.Int('units2', min_value=2, max_value=8000, step=800)
@@ -96,5 +97,5 @@ def get_meta_model(input_shape):
         num_layers = hp.Int('num_layers', min_value=1, max_value=100, step=10)#6
         dim_feedforward = hp.Int('dim_feedforward', min_value=1, max_value=100, step=10)#16
         dropout = hp.float('dropout', min_value=0.01, max_value=1, step=0.02)#0.1
-        return build_model(input_shape, output_shape, units1, units2, units3, activation, num_heads, num_layers, dim_feedforward, dropout)
+        return build_model(input_shape, output_shape, units1=units1, units2=units2, units3=units3, activation=activation, num_heads=num_heads, num_layers=num_layers, dim_feedforward=dim_feedforward, dropout=dropout)
     return meta_model
